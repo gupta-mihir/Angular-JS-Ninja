1ec3021db3b39a38a8939b09e74c111f0947c009
Mihir Gupta <guptamihir@protonmail.com>
2024-04-11 11:11:07 -0400

    Ninja tutorial commits

---
+++ app.js
@@ -0,0 +1,12 @@
+var myNinjaApp = angular.module('myNinjaApp', [])
+myNinjaApp.config(function(){
+
+});
+
+myNinjaApp.run(function(){
+
+});
+
+myNinjaApp.controller('NinjaController', function($scope){
+    $scope.message = "hey y'all";
+});
