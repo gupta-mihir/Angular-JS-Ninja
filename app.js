7b375156b75db429a4de3074d4aa467e8b2714d2
Mihir Gupta <guptamihir@protonmail.com>
2024-04-12 12:08:17 -0400

    Order buttons implemented, header file added

--- app.js
+++ app.js
@@ -7,7 +7,27 @@ myNinjaApp.run(function(){
 
 });
 
-myNinjaApp.controller('NinjaController', function($scope){
-    $scope.message = "hey y'all";
+myNinjaApp.controller('NinjaController', ['$scope', function($scope){
+    $scope.message = "Ninja for Hire!";
     $scope.ninjas = ['Shogun', 'Sekiro', 'Samurai', ]
-});
+    $scope.detailedNinjas = [
+        {
+            name: 'Shogun',
+            belt: 'White',
+            rate: '$10 Million',
+            available: true
+        },
+        {
+            name: 'Sekiro',
+            belt: 'Yellow',
+            rate: '$12 Million',
+            available: true
+        },
+        {
+            name: 'Samurai',
+            belt: 'Black',
+            rate: '$15 Million',
+            available: true
+        }
+    ];
+}]);
