5c8d482c9efad85bd88adc2a3cf045f36baee452
Mihir Gupta <guptamihir@protonmail.com>
2024-04-12 14:22:22 -0400

    Changed this to be a marketplace style website

--- app.js
+++ app.js
@@ -1,19 +1,25 @@
-var myNinjaApp = angular.module('myNinjaApp', [])
-myNinjaApp.config(function(){
+var myMarketplaceApp = angular.module('myMarketplaceApp', [])
+myMarketplaceApp.config(function(){
 
 });
 
-myNinjaApp.run(function(){
+myMarketplaceApp.run(function(){
 
 });
 
-myNinjaApp.controller('NinjaController', ['$scope', function($scope){
-    $scope.message = "Ninja for Hire!";
-    $scope.ninjas = ['Shogun', 'Sekiro', 'Samurai', ]
-    $scope.detailedNinjas = [
+myMarketplaceApp.controller('MarketController', ['$scope', function($scope){
+    $scope.message = "Goods for Sale!";
+    //$scope.ninjas = ['Shogun', 'Sekiro', 'Samurai', ]
+    
+    $scope.removeItem = function(item){
+        var removedItem = $scope.detailedItems.indexOf(item);
+        $scope.detailedItems.splice(removedItem, 1);
+    }
+
+    $scope.detailedItems = [
         {
-            name: 'Shogun',
-            belt: 'White',
+            name: 'Toothbrush',
+            belt: 'Blue',
             rate: '$10 Million',
             available: true
         },
